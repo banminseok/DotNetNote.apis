f89b82acc58c27942138010b75628162b1bd90d2
ban <halfmin@gmail.com>
2021-08-10 14:51:43 +0900

    ServiceController

--- DotNetNote.Apis/Controllers/34_WebAPI/ServiceController.cs
+++ DotNetNote.Apis/Controllers/34_WebAPI/ServiceController.cs
@@ -4,6 +4,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DotNetNote.Apis.Controllers
 {
+    /// <summary>
+    /// 응답 결과 상태 표시
+    /// IActionResult( 혹은  Task<IActionResult>) 반환
+    /// Ok (object) : 200 OK 성공
+    /// BadRequest(ModelState) : 잘못된 요청
+    /// CreatedAtAction ("Get", new {id=1234}) : 201, 새리소스 생성
+    /// Content ("안녕하세요") : 텍스트 반환
+    /// Json (object) : JSON 반환
+    /// </summary>
+    /// 
     [Route("api/[controller]")]
     public class ServiceController : ControllerBase
     {
@@ -18,20 +28,29 @@ namespace DotNetNote.Apis.Controllers
         //[FromQuery] --> ?id=1222
         [HttpGet("{id=1000}")] //기본값
         [HttpGet("{id:int}")] //제약조건
-        public string Get([FromRoute]int id, string query)
+        //public string Get([FromRoute]int id, string query)
+        public IActionResult Get([FromRoute]int id, string query)
         {
-            return $"넘어온 값 : {id}, {query}";
+            //return $"넘어온 값 : {id}, {query}";
+            return Ok(new JsonFormVal { Id = id, Text = $"값 : {id} " });
         }
+
 
         // POST api/<ServiceController>
         [HttpPost]
-        public void Post([FromBody] JsonFormVal value)
+        public IActionResult Post([FromBody] JsonFormVal value)
         {
             if (!ModelState.IsValid)
             {
-                throw new System.InvalidOperationException("잘못된 값");
+                //throw new System.InvalidOperationException("잘못된 값");
+                return BadRequest(ModelState); //400 BadRequest
             }
+            value.Id++;
+            // 데이터 저장 ,
+            return CreatedAtAction("Get",new { id = value.Id }, value);
         }
+
+       
 
         // PUT api/<ServiceController>/5
         [HttpPut("{id}")]
